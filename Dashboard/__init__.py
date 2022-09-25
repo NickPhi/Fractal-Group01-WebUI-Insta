0ea9ce49c9e0af479ddba30e15624a40f356294c
NickPhi <nick_cn@ymail.com>
2022-09-24 20:10:18 -0700

    update2

--- Dashboard/__init__.py
+++ Dashboard/__init__.py
@@ -6,16 +6,14 @@ import subprocess
 from datetime import datetime, date
 import requests
 import re
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import smtplib
 import flask
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-from flask_socketio import SocketIO, send, emit
+# from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, logger=True, engineio_logger=True)
+# socketio = SocketIO(app, logger=True, engineio_logger=True)
 
 import Dashboard.service
 import Dashboard.routes
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from Dashboard import app, os, socketio
+from Dashboard import app, os
 
 if __name__ == "__main__":
     # get_data()
     os.system("sudo /usr/bin/systemctl restart screensaver.service")
-    # app.run(debug=True)
-    socketio.run(app, host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
+    # socketio.run(app, host='127.0.0.1', port=5000)
