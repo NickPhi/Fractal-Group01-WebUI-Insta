e0b3fef156767f001cd92187e64939279f57f755
NickPhi <nick_cn@ymail.com>
2022-09-27 09:09:34 -0700

    update fixing force update response

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from Dashboard import app, os
 if __name__ == "__main__":
     # get_data()
     os.system("sudo /usr/bin/systemctl restart screensaver.service")
+    print("hello")
     app.run(host='127.0.0.1', port=5000, debug=True)
     # socketio.run(app, host='127.0.0.1', port=5000)
