aceb470e6126a9a5fea52e430fb94db3ae58088e
NickPhi <nick_cn@ymail.com>
2022-10-09 11:21:42 -0700

    update

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from Dashboard import app, os
 
 if __name__ == "__main__":
     os.system("sudo /usr/bin/systemctl restart screensaver.service")
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    aapp.run(host='127.0.0.1', port=5000, debug=True)
