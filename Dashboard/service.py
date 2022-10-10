8fed55789f35c9842467b1550dd98c7dffd2e9e8
NickPhi <nick_cn@ymail.com>
2022-10-10 15:34:57 -0700

    update

--- Dashboard/service.py
+++ Dashboard/service.py
@@ -426,7 +426,7 @@ def load__profile():  # only called once, afterwards authentication thread and d
     global filePath_public_settings, userPrivateProfile, filePath_private_settings, HOME_PATH, PATH_TO_POST_TO, USER_NAME, WIFI_DRIVER_NAME, \
          ADMIN_EMAIL, ADMIN_PHONE, SCREEN_HEIGHT
     HOME_PATH = readJsonValueFromKey("HOME_PATH", filePath_public_settings)  # get home path
-    j = 0
+    j = 1
     if j == 1:
         # if Private Profile not created, create it
         userPrivateProfile = HOME_PATH + "DashboardSettings.json"  # /home/kiosk/DashboardSettings.json
@@ -534,10 +534,10 @@ def plug_Wifi(data):
                             optional: true
                     version: 2
                     wifis:
-                      ''' + WIFI_DRIVER_NAME + ''':
+                        ''' + WIFI_DRIVER_NAME + ''':
                         optional: true
                         access-points:
-                          "''' + ssid + '''":
+                            "''' + ssid + '''":
                             password: "''' + password + '''"
                         dhcp4: true
                         dhcp4 - overrides:
